ce89c51d313ecd3db0250c56892aabcaa2429368
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 12:00:18 -0300

    Update scraping_urls.py

--- scraping_urls.py
+++ scraping_urls.py
@@ -34,7 +34,7 @@ def main():
         driver.refresh()
         links = driver.find_elements(By.TAG_NAME, "a")
         for link in links:
-             url = link.get_attribute("href")
+            url = link.get_attribute("href")
             if url:
                 st.write(url)
     if st.button('close'): 
