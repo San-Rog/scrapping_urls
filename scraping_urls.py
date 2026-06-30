8b351d997d338d8a3587893a3574e8aaeceb6801
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 10:11:38 -0300

    Update scraping_urls.py

--- scraping_urls.py
+++ scraping_urls.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 
-st.title("🌐 Streamlit + Selenium Web Scraper")
 @st.cache_resource
 def get_driver():
     options = Options()
@@ -22,17 +21,19 @@ def get_driver():
         options=options
     )
 
-def main():
-    driver = get_driver()
+def main(driver):
     if st.button("Scrapping"):
         driver = get_driver()        
         driver.get("https://www.tjma.jus.br/")
         time.sleep(3)
         texto_da_pagina = driver.find_element(By.TAG_NAME, "body").text
         st.write(texto_da_pagina)
-    if st.button("close"):
-        driver.quit()      
  
 if __name__ == '__main__':
     st.cache_data.clear()
-    main()
+    st.title("🌐 Streamlit + Selenium Web Scraper")
+    driver = get_driver()
+    if st.button("Scrapping"):
+        main(drive)
+    if st.button("close"):
+        driver.quit()    
