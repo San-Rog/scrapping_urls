2b81d05f3b254ab76154428a77d687b00fe05c57
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 11:51:34 -0300

    Update scraping_urls.py

--- scraping_urls.py
+++ scraping_urls.py
@@ -10,25 +10,53 @@ from webdriver_manager.core.os_manager import ChromeType
 @st.cache_resource
 def get_driver():
     options = Options()
-    options.add_argument("--headless=new")  # Required for server deployment
-    options.add_argument("--no-sandbox")   # Required for Linux containers
-    options.add_argument("--disable-dev-shm-usage") # Overcomes limited resource problems
+    options.add_argument("--headless") 
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    
-    # Automatically manages and finds the correct Chromium binary path
     return webdriver.Chrome(
-        service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+        service=Service(ChromeDriverManager().install()), 
         options=options
     )
-
+    
 def main():
-    if st.button("Scrapping"):
+    if st.button("Scrapping Texto"):
         driver = get_driver()
-        driver.get("https://www.tjma.jus.br/")
-        time.sleep(5)
-        texto_da_pagina = driver.find_element(By.TAG_NAME, "body").text
-        st.write(texto_da_pagina)
-        driver.quit()
-
+        try:
+            st.session_state['cont'] += 1
+            if st.session_state['cont'] == 1:
+                driver.get("https://www.tjma.jus.br/")
+            time.sleep(5)
+            driver.refresh()
+            textPages = driver.find_element(By.TAG_NAME, "body").text
+            st.write(textPages)
+        finally:
+            pass
+            #driver.quit()  
+    if st.button("Scrapping Links"):
+        driver = get_driver()
+        try:
+            st.session_state['cont'] += 1
+            if st.session_state['cont'] == 1:
+                driver.get("https://www.tjma.jus.br/")
+            time.sleep(5)
+            driver.refresh()
+            links = driver.find_elements(By.TAG_NAME, "a")
+            for link in links:
+                url = link.get_attribute("href")
+                if url:
+                    st.write(url)
+        finally:
+            pass
+            #driver.quit()  
+    if st.button('close'): 
+        try:
+           driver.quit()
+        except:
+            pass
+    st.write(st.session_state['cont'])
+ 
 if __name__ == '__main__':
+    if 'cont' not in st.session_state:
+        st.session_state['cont'] = 0
     main()
