30363cc2f0a9e9469c694026e26805e895015233
San-Rog <118987989+San-Rog@users.noreply.github.com>
2026-06-30 10:56:45 -0300

    Refactor scraping logic into main function

--- scraping_urls.py
+++ scraping_urls.py
@@ -21,13 +21,14 @@ def get_driver():
         options=options
     )
 
-if st.button("Scrapping"):
-    driver = get_driver()
-    driver.get("https://www.tjma.jus.br/")
-    time.sleep(5)
-    texto_da_pagina = driver.find_element(By.TAG_NAME, "body").text
-    st.write(texto_da_pagina)
-    driver.quit()
+def main():
+    if st.button("Scrapping"):
+        driver = get_driver()
+        driver.get("https://www.tjma.jus.br/")
+        time.sleep(5)
+        texto_da_pagina = driver.find_element(By.TAG_NAME, "body").text
+        st.write(texto_da_pagina)
+        driver.quit()
 
 if __name__ == '__main__':
     main()
